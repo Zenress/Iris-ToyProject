117dbc4770d919c1554dbf7af04b08589ed2ed2e
Shano <xshfo1806@dsb.dk>
2022-07-18 15:24:38 +0200

    Better UX, more secure and more compact @ 15:24

--- source/models/IrisClassification-Prediction.py
+++ source/models/IrisClassification-Prediction.py
@@ -6,15 +6,14 @@ import yaml
 with open("configuration/config.yaml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-
-loaded_model = pickle.load(open("models/" + cfg["model_name"], 'rb'))
-mappings_file = np.load("models/" + cfg["encoder_mappings"], allow_pickle=True)
+loaded_dictionary = pickle.load(open("models/" + cfg["model_and_encoder_name"], 'rb'))
+mappings_file = loaded_dictionary["encoder_mappings"]
 
 def prediction(): 
     """_summary_
     Conditions checked for:
     Datatype: Float
-    Length: 4 floats seperated by ,
+    Length: 1 number for each iteration
     Maximum: [7.9, 4.4, 6.9, 2.5] Depending on iteration
     Minimum: [4.3, 2, 1, 0.1] Depending on iteration
     Along with user instructions to follow
@@ -22,49 +21,36 @@ def prediction():
     Try:
      Checks for different input requirements
     Raises:
-        ValueError: _description_
+        ValueError: Raises a valueerror when you type wrong datatype or too low / high number
     """
-    print("Write the data you want to be predicted:")
-    print("Input Format: Sepal Length, Sepal Width, Petal Length and Petal Width")
-    print("Datatype: Float")
-    print("Highest Possible Number: 10")
-    print("Lowest Possible Number: 0.1")
-    unedited_input = input("Write here: ")
-    edited_input = unedited_input.split(',')
-    
-    i=0 
-    try:
-        for number in edited_input:
-            if float(number) >= cfg["input_min_values"][i] and float(number) <= cfg["input_max_values"][i]:  
-                i+=1
-                continue
-            else:
-                raise ValueError()
+    edited_input = []
+    while len(edited_input) != len(cfg["features"]):
+        for feature_nr in range(len(cfg["features"])):
+            print(f"Write the data you want to be predicted {feature_nr+1}/4:")
+            print(f"Input Feature: {cfg['features'][feature_nr]}")
+            print("Datatype: Float")
+            print(f"Highest Possible Number: {cfg['input_max_values'][feature_nr]}")
+            print(f"Lowest Possible Number: {cfg['input_min_values'][feature_nr]}")
+            user_input = input("Write here: ")
+            print("____________________________________________")
         
-        if len(edited_input) == 4:
-            class_value = loaded_model.predict(np.reshape(edited_input,(1,4)))
-            print(class_value,'=',mappings_file[class_value[0]])
+            try:            
+                if float(user_input) >= cfg['input_min_values'][feature_nr] and float(user_input) <= cfg['input_max_values'][feature_nr]:  
+                    edited_input.append(user_input)
+                else:
+                    raise ValueError()
+                
+            except ValueError:
+                print("___________________________________________________")
+                print(f"Please enter a Float that's between {cfg['input_min_values'][feature_nr]} and {cfg['input_max_values'][feature_nr]}\n \n \n")
+                break
             
-        else:
-            print("You have not fulfilled the format requirements")
-            print("Try Again \n \n \n")
-            prediction()
-            
-    except ValueError:
-        print("Please enter a Float that's between 0.1 and 11 \n \n \n ")
-        prediction()
+    class_value = loaded_dictionary["model"].predict(np.reshape(edited_input,(1,4)))
+    print(class_value,'=',mappings_file[class_value[0]])
 
 prediction()
-#Make sure the highest and lowest number match the iteration
-#Iterate through the columns instead of a comma seperated list
-#label is not labels as there is only one label
-#label name in config, instead of in code
-#Features in config instead of in code
 #Follow more machine learning naming conventions
-#Update readme with new file distribution
 #explanation of configuration file?
 #Avoid casting.
 #Make code shorter using configuration
-#Encoder included with model file?
-#Change the way the Encoder is saved and loaded
 #Review what the zip function does
