ce4096aed9e8f0ac6ff5259242635956d790fb7a
Zenress <64189421+Zenress@users.noreply.github.com>
2022-07-22 14:58:20 +0200

    updated todo list

--- source/models/iris_classification_prediction.py
+++ source/models/iris_classification_prediction.py
@@ -88,6 +88,4 @@ if __name__ == "__main__":
 #TODO: Make sure functions are 1 logical function only
 #TODO: Create more functions if there are too many logical functions in a function
 #TODO: Look through Readme file to see if there is anything that has to be changed after code refactoring
-#TODO: Update run command names in Readme
-#TODO: Complete all docstrings for functions
 #TODO: Make sure there aren't too many logical things done inside main function alone. They should be seperated into functions if possible
