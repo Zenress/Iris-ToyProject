109638a64fba4c5a51d535a5fb71c0e1847bf586
Zenress <hotrockgaming@gmail.com>
2022-07-28 09:10:11 +0200

    black code formatter used

--- source/models/prediction.py
+++ source/models/prediction.py
@@ -6,8 +6,8 @@ import pickle
 import numpy as np
 import yaml
 
-MODEL_PATH = 'models/'
-CONFIG_PATH = 'configuration/config.yaml'
+MODEL_PATH = "models/"
+CONFIG_PATH = "configuration/config.yaml"
 
 
 def value_check(feature_dict: dict) -> list:
@@ -43,13 +43,18 @@ def value_check(feature_dict: dict) -> list:
             print("____________________________________________")
 
             try:
-                if float(user_input) >= value['min'] and float(user_input) <= value['max']:
+                if (
+                    float(user_input) >= value["min"]
+                    and float(user_input) <= value["max"]
+                ):
                     edited_input.append(user_input)
                 else:
                     raise ValueError()
 
             except ValueError:
-                print(f"Please enter a Float that's between {value['min']} and {value['max']}")
+                print(
+                    f"Please enter a Float that's between {value['min']} and {value['max']}"
+                )
                 break
 
     return edited_input
@@ -66,8 +71,12 @@ def prediction(user_input: list, model_encoder_dictionary: dict):
         user_input (list): user input edited into a 4 item list.
         model_encoder_dictionary (dict): dictionary with the model and encoder objects.
     """
-    class_value = model_encoder_dictionary["model"].predict(np.reshape(user_input,(1,4)))
-    print(class_value, '=', model_encoder_dictionary['encoder_mappings'][class_value[0]])
+    class_value = model_encoder_dictionary["model"].predict(
+        np.reshape(user_input, (1, 4))
+    )
+    print(
+        class_value, "=", model_encoder_dictionary["encoder_mappings"][class_value[0]]
+    )
 
 
 def main():
@@ -82,19 +91,19 @@ def main():
     Predicting using the value_check result and the encoder mappings.
     """
     config_full_path = Path(CONFIG_PATH)
-    with open(config_full_path, "r", encoding='UTF-8') as ymlfile:
+    with open(config_full_path, "r", encoding="UTF-8") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
     model_and_encoder_full_path = Path(MODEL_PATH + cfg["model_and_encoder_name"])
-    model_encoder_dictionary = pickle.load(open(model_and_encoder_full_path, 'rb'))
+    model_encoder_dictionary = pickle.load(open(model_and_encoder_full_path, "rb"))
 
     user_input = value_check(feature_dict=cfg["features"])
 
     prediction(user_input=user_input, model_encoder_dictionary=model_encoder_dictionary)
 
+
 if __name__ == "__main__":
     main()
 
-#TODO: Finish training.py main function docstring
-#TODO: LAST. Formulate a plan for how to get project to MLOPS level. 1
+# TODO: LAST. Formulate a plan for how to get project to MLOPS level. 1
 
