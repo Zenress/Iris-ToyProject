093bdd82e59114916c691af85017faf958b04d4d
Shano <xshfo1806@dsb.dk>
2022-07-29 11:54:37 +0200

    add type hinting, path corrected, name correction

--- source/visualization/data_analysis.py
+++ source/visualization/data_analysis.py
@@ -12,10 +12,10 @@ REPORT_PATH = 'reports/'
 CONFIG_PATH = 'configuration/config.yaml'
 
 def file_report(
-    altered_data_df,
-    report_title,
-    report_name
-    ):
+    data_df: pd.DataFrame,
+    report_title: str,
+    report_name: str
+    ) -> None:
     """
     Write profile report.
 
@@ -27,13 +27,13 @@ def file_report(
         report_title (str) string gotten from the dictionary it is in.
         report_name (str) name of the report gotten from the configuration file.
     """
-    report_full_path = Path(REPORT_PATH + report_name)
-    data_profile = pp.ProfileReport(altered_data_df,
+    report_full_path = Path(REPORT_PATH, report_name)
+    data_profile = pp.ProfileReport(data_df,
                                     title=report_title,
                                     explorative=True)
     data_profile.to_file(report_full_path)
 
-def main():
+def main() -> None:
     """
     Execute on initialization.
 
@@ -43,11 +43,10 @@ def main():
     reading the dataset and assigning it to a pandas.DataFrame ->,
     running the file_report function.
     """
-    config_full_path = Path(CONFIG_PATH)
-    with open(config_full_path, "r", encoding='UTF-8') as ymlfile:
+    with open(CONFIG_PATH, "r", encoding='UTF-8') as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
-    dataset_full_path = Path(DATASET_PATH + cfg["dataset_name"])
+    dataset_full_path = Path(DATASET_PATH, cfg["dataset_name"])
 
     #Assigning custom column headers while reading the csv file
     data_df = pd.read_csv(
@@ -58,7 +57,7 @@ def main():
     print(data_df.head(10))
 
     file_report(
-        altered_data_df=data_df,
+        data_df=data_df,
         report_title=cfg["report_settings"]["title"],
         report_name=cfg["iris_analysis_report_name"]
         )
